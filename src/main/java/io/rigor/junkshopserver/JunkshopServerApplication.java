4ab0e384ef58c928eb988dcbd0d4ecb76a7698fa
rigorei <rigorei.sarmiento@gmail.com>
2019-11-18 21:06:16 +0800

    Initial commit

---
+++ src/main/java/io/rigor/junkshopserver/JunkshopServerApplication.java
@@ -0,0 +1,13 @@
+package io.rigor.junkshopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunkshopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunkshopServerApplication.class, args);
+	}
+
+}
