cd6bda98e694a9426abb65943e7b493e527d8ad8
rigorei <rigorei.sarmiento@gmail.com>
2020-01-03 14:07:59 +0800

    Mega cheat

--- src/main/java/io/rigor/junkshopserver/cash/DynamoCashSwindler.java
+++ src/main/java/io/rigor/junkshopserver/cash/DynamoCashSwindler.java
@@ -6,21 +6,33 @@ import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.util.TableUtils;
 import io.rigor.junkshopserver.expense.Expense;
+import io.rigor.junkshopserver.expense.ExpenseService;
 import io.rigor.junkshopserver.junk.Junk;
+import io.rigor.junkshopserver.junk.JunkService;
 import io.rigor.junkshopserver.sale.Sale;
+import io.rigor.junkshopserver.sale.SaleService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class DynamoCashSwindler implements CashService {
   private CashRepository cashRepository;
+  private JunkService junkService;
+  private SaleService<Sale> saleService;
+  private ExpenseService expenseService;
 
   public DynamoCashSwindler(CashRepository cashRepository,
-                            AmazonDynamoDB amazonDynamoDB) {
+                            AmazonDynamoDB amazonDynamoDB,
+                            JunkService junkService,
+                            SaleService<Sale> saleService, ExpenseService expenseService) {
     this.cashRepository = cashRepository;
+    this.junkService = junkService;
+    this.saleService = saleService;
+    this.expenseService = expenseService;
     DynamoDBMapper mapper = new DynamoDBMapper(amazonDynamoDB);
     CreateTableRequest tableRequest = mapper.generateCreateTableRequest(Cash.class);
     tableRequest.setProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
@@ -34,7 +46,7 @@ public class DynamoCashSwindler implements CashService {
 
   @Override
   public void addSales(Sale sale) {
-    String totalPrice = sale.getTotalPrice();
+ /*   String totalPrice = sale.getTotalPrice();
     String date = sale.getDate();
     Optional<Cash> byDate = cashRepository.findByDate(date);
     if (byDate.isPresent()) {
@@ -48,12 +60,13 @@ public class DynamoCashSwindler implements CashService {
       cash.setSales(totalPrice);
       cash.setCashOnHand("" + getCashOnHand(cash));
       cashRepository.save(cash);
-    }
+    }*/
+    calibrateAll();
   }
 
   @Override
   public void addPurchases(Junk junk) {
-    String totalPrice = junk.getTotalPrice();
+/*    String totalPrice = junk.getTotalPrice();
     String date = junk.getDate();
     Optional<Cash> byDate = cashRepository.findByDate(date);
     if (byDate.isPresent()) {
@@ -67,7 +80,36 @@ public class DynamoCashSwindler implements CashService {
       cash.setPurchases(totalPrice);
       cash.setCashOnHand("" + getCashOnHand(cash));
       cashRepository.save(cash);
-    }
+    }*/
+    calibrateAll();
+  }
+
+  public void calibrateAll() {
+    List<Cash> cashList = allDailyCash();
+    List<String> dates = cashList.stream().map(Cash::getDate).collect(Collectors.toList());
+    List<Cash> updatedKaching = dates.stream()
+        .map(date -> {
+          Optional<Cash> junkByDate = cashRepository.findByDate(date);
+
+          List<Junk> junk = junkService.findByDate(date);
+          double purchasesSum = junk.stream().mapToDouble(j -> Double.valueOf(j.getTotalPrice())).sum();
+
+          List<Sale> sales = saleService.findByDate(date);
+          double salesSum = sales.stream().mapToDouble(s -> Double.valueOf(s.getTotalPrice())).sum();
+
+          List<Expense> expenses = expenseService.findByDate(date);
+          double expenseSum = expenses.stream().mapToDouble(e -> Double.valueOf(e.getAmount())).sum();
+
+
+          Cash cash = junkByDate.orElseGet(Cash::new);
+          cash.setSales("" + salesSum);
+          cash.setPurchases("" + purchasesSum);
+          cash.setExpenses("" + expenseSum);
+          cash.setCashOnHand("" + getCashOnHand(cash));
+          return cash;
+        })
+        .collect(Collectors.toList());
+    cashRepository.saveAll(updatedKaching);
   }
 
   /**
@@ -75,7 +117,7 @@ public class DynamoCashSwindler implements CashService {
    */
   @Override
   public void addExpense(Expense expense) {
-    String amount = expense.getAmount();
+  /*  String amount = expense.getAmount();
     String date = expense.getDate();
     Optional<Cash> byDate = cashRepository.findByDate(date);
     if (byDate.isPresent()) {
@@ -88,12 +130,13 @@ public class DynamoCashSwindler implements CashService {
       cash.setExpenses(amount);
       cash.setCashOnHand("" + getCashOnHand(cash));
       cashRepository.save(cash);
-    }
+    }*/
+    calibrateAll();
   }
 
   @Override
   public void deleteExpense(Expense expense) {
-    String amount = expense.getAmount();
+  /*  String amount = expense.getAmount();
     String date = expense.getDate();
     Optional<Cash> byDate = cashRepository.findByDate(date);
     if (byDate.isPresent()) {
@@ -106,7 +149,8 @@ public class DynamoCashSwindler implements CashService {
       cash.setExpenses(amount);
       cash.setCashOnHand("" + getCashOnHand(cash));
       cashRepository.save(cash);
-    }
+    }*/
+    calibrateAll();
   }
 
   @Override
@@ -117,11 +161,15 @@ public class DynamoCashSwindler implements CashService {
     if (byDate.isPresent()) {
       Cash c = byDate.get();
       c.setCapital(capital);
-      c.setCashOnHand("" +(getCashOnHand(c)));
-      return cashRepository.save(c);
+      c.setCashOnHand("" + (getCashOnHand(c)));
+      Cash save = cashRepository.save(c);
+      calibrateAll();
+      return save;
     } else {
       cash.setCashOnHand("" + getCashOnHand(cash));
-      return cashRepository.save(cash);
+      Cash save = cashRepository.save(cash);
+      calibrateAll();
+      return save;
     }
   }
 
@@ -144,7 +192,7 @@ public class DynamoCashSwindler implements CashService {
     cash.setDate(date);
     LocalDate dateBefore = LocalDate.parse(date).minusDays(1L);
     Optional<Cash> yesterdayCash = cashRepository.findByDate(dateBefore.toString());
-    if (yesterdayCash.isPresent()){
+    if (yesterdayCash.isPresent()) {
       Cash yesterday = yesterdayCash.get();
       String capital = yesterday.getCashOnHand();
       cash.setCapital(capital);
